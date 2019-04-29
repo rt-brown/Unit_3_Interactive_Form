593befd223474da58fd95d11530db323124943b5
rt-brown <47324919+rt-brown@users.noreply.github.com>
2019-04-29 14:00:59 -0700

    Added conditionality for other text box

--- Unit_3_Interactive_Form/js/app.js
+++ Unit_3_Interactive_Form/js/app.js
@@ -1,3 +1,15 @@
-$( document ).ready(function() {
-    $( "#name" ).focus();
-  });
+refreshFocus = () => {
+    $('#name').focus();
+    $('#other-title').hide()
+};
+refreshFocus();
+
+const jobRole = $('#title');
+
+jobRole.on('change', function (){
+    if(jobRole.val() === 'other'){
+        $('#other-title').show();
+        }else {
+        $('#other-title').hide()
+}
+});
