df503192c77498d967b71fdc9446472e6be70fe4
rt-brown <47324919+rt-brown@users.noreply.github.com>
2019-05-10 15:05:48 -0700

    updated credit card regex

--- Unit_3_Interactive_Form/js/app.js
+++ Unit_3_Interactive_Form/js/app.js
@@ -234,7 +234,8 @@ submitButton.on('click', function(){
         if (paymentSelection.val() != 'credit card') {
             return true
         } else if (paymentSelection.val() === 'credit card') {
-            const regex = /^(?:4[0-9]{12}(?:[0-9]{3})?|[25][1-7][0-9]{14}|6(?:011|5[0-9][0-9])[0-9]{12}|3[47][0-9]{13}|3(?:0[0-5]|[68][0-9])[0-9]{11}|(?:2131|1800|35\d{3})\d{11})$/
+            const regex = /\b(?:\d[ -]*?){13,16}\b/
+
             const cardNumberinput = $('#cc-num').val()
             return(regex.test(cardNumberinput));
         } 
